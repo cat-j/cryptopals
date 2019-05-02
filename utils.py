b0cc8f379760aeb5cee07a4df48820366e418539
cat-j <catalinajuarros@gmail.com>
2019-05-02 16:38:22 -0300

    Add fixed XOR function

--- utils.py
+++ utils.py
@@ -3,3 +3,6 @@ import base64
 # @var my_string: ASCII representation of hex bytes without 0x prefix
 def str_to_base64(my_string):
     return base64.b64encode(bytes.fromhex(my_string))
+
+def fixed_xor(buf1, buf2):
+    return hex(int(buf1, 16) ^ int(buf2, 16))
