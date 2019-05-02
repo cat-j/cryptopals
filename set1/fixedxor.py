12636560c7a5b0aa16fe4b60cdca9eafd602c497
cat-j <catalinajuarros@gmail.com>
2019-05-02 16:36:26 -0300

    Solve fixed XOR challenge

--- set1/fixedxor.py
+++ set1/fixedxor.py
@@ -1,7 +1,10 @@
+import binascii
 import importlib.util
 spec = importlib.util.spec_from_file_location("utils", "./utils.py")
 utils = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(utils)
 
-raw_bytes = bytearray.fromhex("1c0111001f010100061a024b53535009181c")
-print(raw_bytes)
+raw_bytes_1 = int("1c0111001f010100061a024b53535009181c", 16)
+raw_bytes_2 = int("686974207468652062756c6c277320657965", 16)
+xord = raw_bytes_1 ^ raw_bytes_2
+print(hex(xord))
