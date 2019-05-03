c35123e6e81a75b697fe2a45e22c43615b672be2
cat-j <catalinajuarros@gmail.com>
2019-05-03 15:24:42 -0300

    Decrypt using ASCII analysis

--- cryptopals/detectxor.py
+++ cryptopals/detectxor.py
@@ -3,7 +3,14 @@ import singlebytexor
 def find_single_byte_xor(filename):
     lines = open(filename, 'r').read().splitlines()
     raw_data = [bytearray.fromhex(l) for l in lines]
-    decrypted = [singlebytexor.frequency_decrypt(r) for r in raw_data]
-    print(decrypted)
+    best_score, best_string = 0, ""
 
-find_single_byte_xor("data/4.txt")
+    for r in raw_data:
+        current_score, current_string = singlebytexor.ascii_chars_decrypt(r)
+        if (current_score > best_score):
+            print("score: %f\tstring: %s\n" % (current_score, current_string))
+            best_score, best_string = current_score, current_string
+    
+    return (best_score, best_string)
+
+print(find_single_byte_xor("data/4.txt"))
