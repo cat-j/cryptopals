1fe26fa00ece7072580736904f7b9fb598e14d3c
cat-j <catalinajuarros@gmail.com>
2019-05-07 12:48:28 -0300

    Take bytes class arguments

--- cryptopals/encrypt.py
+++ cryptopals/encrypt.py
@@ -1,18 +1,22 @@
 import binascii
 
 def xor_encrypt(plaintext, key):
-    plaintext_bytes = bytes(plaintext, "utf-8")
-    key_bytes = bytes(key, "utf-8")
+    if (isinstance(plaintext, str)):
+        plaintext = bytes(plaintext, "utf-8")
+    if (isinstance(key, str)):
+        key = bytes(key, "utf-8")
+
     key_length = len(key)
     i = 0
     ciphertext = bytearray(b'')
     
-    for b in plaintext_bytes:
-        ciphertext.append(b ^ key_bytes[i%key_length])
+    for b in plaintext:
+        ciphertext.append(b ^ key[i%key_length])
         i += 1
 
     return ciphertext
 
-plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
-encrypted = xor_encrypt(plaintext, "ICE")
-print(binascii.hexlify(encrypted))
+# usage example
+# plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+# encrypted = xor_encrypt(plaintext, "ICE")
+# print(binascii.hexlify(encrypted))
