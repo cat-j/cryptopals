d69a9e22fde0a5132822df6791dd321aedc66833
cat-j <catalinajuarros@gmail.com>
2019-05-08 12:35:11 -0300

    Improve English frequency analysis

--- cryptopals/frequency.py
+++ cryptopals/frequency.py
@@ -1,46 +1,47 @@
 import math
 
-ENGLISH_ALPHABET = "abcdefghijklmnopqrstuvwxyz"
-
-ENGLISH_ALPHABET_LETTERS = 26
+ENGLISH_ALPHABET_EXTRA = "abcdefghijklmnopqrstuvwxyz -"
 
 # defined here so we won't have to read a .csv every time
 # unless there's a dramatic shift in English morphology in the near future,
 # this will probably just remain static so there's nothing really wrong
 # with hardcoding it
 ENGLISH_LETTER_FREQUENCIES = {
-    'a': 0.08167,
-    'b': 0.01492,
-    'c': 0.02782,
-    'd': 0.04253,
-    'e': 0.12702,
-    'f': 0.02228,
-    'g': 0.02015,
-    'h': 0.06094,
-    'i': 0.06966,
-    'j': 0.00153,
-    'k': 0.00772,
-    'l': 0.04025,
-    'm': 0.02406,
-    'n': 0.06749,
-    'o': 0.07507,
-    'p': 0.01929,
-    'q': 0.00095,
-    'r': 0.05987,
-    's': 0.06327,
-    't': 0.09056,
-    'u': 0.02758,
-    'v': 0.00978,
-    'w': 0.02360,
-    'x': 0.00150,
-    'y': 0.01974,
-    'z': 0.00074
+    'a': 0.0651738,
+    'b': 0.0124248,
+    'c': 0.0217339,
+    'd': 0.0349835,
+    'e': 0.1041442,
+    'f': 0.0197881,
+    'g': 0.0158610,
+    'h': 0.0492888,
+    'i': 0.0558094,
+    'j': 0.0009033,
+    'k': 0.0050529,
+    'l': 0.0331490,
+    'm': 0.0202124,
+    'n': 0.0564513,
+    'o': 0.0596302,
+    'p': 0.0137645,
+    'q': 0.0008606,
+    'r': 0.0497563,
+    's': 0.0515760,
+    't': 0.0729357,
+    'u': 0.0225134,
+    'v': 0.0082903,
+    'w': 0.0171272,
+    'x': 0.0013692,
+    'y': 0.0145984,
+    'z': 0.0007836,
+    ' ': 0.1918182,
+    '-': 0.0651738
 }
 
 def get_empty_dict():
     result = {}
-    for i in range(ENGLISH_ALPHABET_LETTERS):
-        result[chr(ord('a') + i)] = 0
+    for ch in ENGLISH_ALPHABET_EXTRA:
+        result[ch] = 0
+    result['other'] = 0
     return result
 
 def analyse_frequencies(my_string):
@@ -53,8 +54,10 @@ def analyse_frequencies(my_string):
     lowercase_string = my_string.lower()
 
     for c in lowercase_string:
-        if (ord(c) >= ord('a') and ord(c) <= ord('z')):
+        if (ord(c) >= ord('a') and ord(c) <= ord('z')) or c == ' ':
             appearances[c] += 1
+        else:
+            appearances['-'] += 1
 
     # calculate frequencies
     frequencies = {}
@@ -90,7 +93,7 @@ class CharTypeCount:
 
 # @var my_dict: dictionary containing character frequencies in a string
 def dict_frequency_distance(my_dict):
-    return sum([abs(my_dict[k] - ENGLISH_LETTER_FREQUENCIES[k]) for k in ENGLISH_ALPHABET])
+    return sum([(my_dict[k] - ENGLISH_LETTER_FREQUENCIES[k])**2 for k in ENGLISH_ALPHABET_EXTRA])
 
 def is_alpha(ch):
     return ((ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z'))
