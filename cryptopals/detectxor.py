0e7a304457480a53b7fb7fc31784bb6a23bcc888
cat-j <catalinajuarros@gmail.com>
2019-05-08 13:09:31 -0300

    Format

--- cryptopals/detectxor.py
+++ cryptopals/detectxor.py
@@ -1,6 +1,5 @@
 import singlebytexor
 import frequency
-import binascii
 
 def find_single_byte_xor(filename):
     lines = open(filename, 'r').read().splitlines()
@@ -9,9 +8,9 @@ def find_single_byte_xor(filename):
 
     for r in raw_data:
         current_score, current_string = singlebytexor.alpha_chars_decrypt(r)
-        if (current_score > best_score):
+        if current_score > best_score:
             best_score, best_string = current_score, current_string
-    
+
     return best_string
 
 print(find_single_byte_xor("data/4.txt"))
