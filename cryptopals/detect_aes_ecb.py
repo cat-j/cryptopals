9497f49a03f090137f3500a39f627c4b8fc71ffd
cat-j <catalinajuarros@gmail.com>
2019-05-13 11:47:55 -0300

    Solve challenge 8

--- cryptopals/detect_aes_ecb.py
+++ cryptopals/detect_aes_ecb.py
@@ -1,8 +1,27 @@
 import fileutils
 import coreutils
 
+def has_equal_blocks(blocks):
+    l = len(blocks)
+    sorted_blocks = blocks.copy()
+    sorted_blocks.sort()
+    
+    for i in range(l-1):
+        if sorted_blocks[i] == sorted_blocks[i+1]:
+            return True
+    
+    return False
+
 def find_aes_ecb(filename):
     raw_data = fileutils.decode_file_hex(filename)
-    print(raw_data)
 
-find_aes_ecb("data/8.txt")
+    for line in raw_data:
+        blocks = coreutils.get_blocks(line, 16)
+        if has_equal_blocks(blocks):
+            return blocks
+    
+    # No patterns found
+    return None
+
+aes_encrypted = find_aes_ecb("data/8.txt")
+print(aes_encrypted)
