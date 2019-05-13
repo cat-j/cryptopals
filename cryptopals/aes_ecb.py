0568e5da4f439f6ecbdcf2113d9e73e653fbb84d
cat-j <catalinajuarros@gmail.com>
2019-05-13 11:13:03 -0300

    Solve challenge 7

--- cryptopals/aes_ecb.py
+++ cryptopals/aes_ecb.py
@@ -1,20 +1,32 @@
 import fileutils
 import math
 
-from cryptography.hazmat.primitives.aead import AESCCM
+from Crypto.Cipher import AES
 
-def aes_ecb_decrypt(ciphertext, key):
+def get_blocks(ciphertext, key):
     l = len(ciphertext)
     block_size = len(key)
     num_blocks = math.ceil(l // block_size)
-    plaintext = bytearray(b'')
+    blocks = []
 
     for i in range(num_blocks):
         lower = i*block_size
         upper = min(l, lower+block_size)
-        block = ciphertext[lower:upper]
-        # plaintext += cryptography.hazmat.primitives.ciphers.aead
+        blocks.append(ciphertext[lower:upper])
+    
+    return blocks
+
+def aes_ecb_decrypt(blocks, key):
+    plaintext = bytearray(b'')
+    cipher = AES.new(key, AES.MODE_ECB)
+    
+    for b in blocks:
+        plaintext += cipher.decrypt(b)
+    
+    return plaintext
 
 key = bytearray(b'YELLOW SUBMARINE')
 decoded_bytes = fileutils.decode_file_base64("data/7.txt")
-aes_ecb_decrypt(decoded_bytes, key)
+blocks = get_blocks(decoded_bytes, key)
+plaintext = aes_ecb_decrypt(blocks, key)
+print(plaintext.decode())
